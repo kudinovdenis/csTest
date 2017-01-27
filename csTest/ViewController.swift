506dd6f8da3a183c39d24634f720ba248533a638
Denis Kudinov <Denis.Kudinov@acronis.com>
2017-01-27 18:38:21 +0300

    Initial Commit

---
+++ csTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  csTest
+//
+//  Created by Denis Kudinov on 27/01/2017.
+//  Copyright © 2017 Denis Kudinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
